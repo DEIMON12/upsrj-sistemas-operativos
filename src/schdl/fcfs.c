0c4ac5c4f1425c7b0e80580f6f70d9273515c723
DEIMON12 <deimon12.zosa.gomes@gmail.com>
2026-01-08 16:41:02 -0600

    Tarea lcls

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -10,8 +10,26 @@ int main() {
     read_processes(p, n);
     init_processes(p, n);
 
-    // TODO: Aquí va la lógica del scheduler
-    
+    // Lógica FCFS
+    int current_time = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        // Si el proceso aún no llega, la CPU espera
+        if (current_time < p[i].arrival_time) {
+            current_time = p[i].arrival_time;
+        }
+
+        // Tiempo de espera
+        p[i].waiting_time = current_time - p[i].arrival_time;
+
+        // Tiempo de retorno
+        p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
+
+        // Avanza el tiempo actual
+        current_time += p[i].burst_time;
+    }
+
     print_results(p, n, "FCFS Scheduling");
     return 0;
 }
